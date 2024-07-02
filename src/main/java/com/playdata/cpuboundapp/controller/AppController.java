be9b13ac58a809429a2fcd0db28c5009a83d41cb
kim-yebeen <153195054+kim-yebeen@users.noreply.github.com>
2024-07-02 11:22:21 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-         return "helloooooooooooo what  a day  " ;
+         return "helloooooooooooo hungy girlllll  " ;
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
